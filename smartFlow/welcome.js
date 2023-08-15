41710b9776d2adfeada0aeff457e49c3e28f7bce
leifermendez <leifer33@gmail.com>
2023-08-15 10:18:22 +0200

    new version quue

---
+++ smartFlow/welcome.js
@@ -0,0 +1,21 @@
+const { addKeyword, EVENTS } = require("@bot-whatsapp/bot");
+
+const LIMIT_TEXT = parseInt(process.env.LIMIT_TEXT ?? 800)
+
+module.exports = addKeyword(EVENTS.WELCOME)
+    .addAnswer("⏱️")
+    .addAction(async (ctx, ctxFn) => {
+        const employeesAddon = ctxFn.extensions.employeesAddon
+        const text = ctx.body;
+        const currentState = ctxFn.state.getMyState();
+        const txt = currentState?.answer ?? ""
+        if (txt.length > LIMIT_TEXT) {
+            ctxFn.state.update({ answer: '' });
+        }
+
+        const fullSentence = `${currentState?.answer ?? ""}. ${text}`;
+        const { employee, answer } = await employeesAddon.determine(fullSentence);
+        ctxFn.state.update({ answer });
+        if (employee) employeesAddon.gotoFlow(employee, ctxFn);
+        if (!employee) ctxFn.gotoFlow(flowNotEmployeeWelcome);
+    });
